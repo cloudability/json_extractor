8c04cbeeccfedb939b1f9383a82138ecc0841c23
Brad Heller <brad@cloudability.com>
2013-06-30 09:49:55 -0700

    A few extra test cases.

--- lib/json_extractor.rb
+++ lib/json_extractor.rb
@@ -1,3 +1,9 @@
 require 'json'
 $:.unshift(File.dirname(__FILE__))
 require 'json_extractor/json_extractor'
+
+module JSONExtractor
+  def self.subdocument(filename, key)
+    JSON.parse(extract_subdocument(filename, key))
+  end
+end
--- test/test_json_extractor.rb
+++ test/test_json_extractor.rb
@@ -10,4 +10,14 @@ class TestJsonExtractor < Test::Unit::TestCase
     extracted = JSONExtractor.extract_subdocument(fixture_path("nested_document"), "my_key")
     assert(extracted =~ /\s*{\s+"my_other_key":\s+{\s+"something": 1\s+}\s+}/, "extracted #{extracted.inspect}")
   end
+
+  def test_extracts_extra_nested_json
+    extracted = JSONExtractor.extract_subdocument(fixture_path("extra_nested_document"), "some_key")
+    assert(extracted =~ /\s*{\s+"my_key":\s+{\s+"my_other_key":\s+{\s+"something": 1\s+}\s+}\s+}/, "extracted #{extracted.inspect}")
+  end
+
+  def test_extracts_nested_keys
+    extracted = JSONExtractor.extract_subdocument(fixture_path("extra_nested_document"), "my_other_key")
+    assert(extracted =~ /\s*{\s+"something": 1\s+}/, "extracted #{extracted.inspect}")
+  end
 end
